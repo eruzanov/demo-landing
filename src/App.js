228bdf735344f2efbdd95b3e89783744f2b31a94
eruzanov <evgeniy.ruzanov@piano.io>
2019-06-17 17:11:29 +0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {PureComponent, createRef} from 'react';
+import Greeting from './Greeting';
+import Boards from './Boards';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+    ref = createRef();
+
+    state = {
+        scrollTop: 0
+    };
+
+    onScroll = () => {
+        this.setState({scrollTop: window.pageYOffset || document.documentElement.scrollTop})
+        console.log(this.state)
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll);
+        console.log('componentDidMount', this.ref.current.offsetHeight);
+        this.setState({heightGreeting: this.ref.current.offsetHeight});
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+    }
+
+    render() {
+        return (
+            <>
+                <Greeting ref={this.ref}/>
+                <Boards active={this.state.scrollTop >= this.state.heightGreeting}/>
+            </>
+        );
+    }
 }
 
 export default App;
