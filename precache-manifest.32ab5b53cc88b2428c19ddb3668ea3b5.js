25df41372beeb0ec48b971e64fb7c2173ef3a958
eruzanov <evgeniy.ruzanov@piano.io>
2019-06-17 18:11:22 +0300

    Updates

---
+++ precache-manifest.32ab5b53cc88b2428c19ddb3668ea3b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab442418b80bfb04457cb7f598fa80f5",
+    "url": "/demo-landing/index.html"
+  },
+  {
+    "revision": "d63c8ab638529fa124d0",
+    "url": "/demo-landing/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d082dccca7ef4ef74625",
+    "url": "/demo-landing/static/js/2.9c33b8a0.chunk.js"
+  },
+  {
+    "revision": "d63c8ab638529fa124d0",
+    "url": "/demo-landing/static/js/main.64b3503c.chunk.js"
+  },
+  {
+    "revision": "be0294466f1e3e30b99a",
+    "url": "/demo-landing/static/js/runtime~main.1234c6b4.js"
+  },
+  {
+    "revision": "c135380ff4b27681b18e513538b97457",
+    "url": "/demo-landing/static/media/chart.c135380f.png"
+  },
+  {
+    "revision": "01dedef94ac390f8925705c71f388ee0",
+    "url": "/demo-landing/static/media/chart2.01dedef9.png"
+  },
+  {
+    "revision": "3b90db7e6d383d5edf21d37625939de6",
+    "url": "/demo-landing/static/media/chart3.3b90db7e.png"
+  },
+  {
+    "revision": "61bb53a468bd2d9ddac7f0839554bcdd",
+    "url": "/demo-landing/static/media/chart4.61bb53a4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5d9c3414e06264bb4e93f9a5b55fcfa.js"
+  "/demo-landing/precache-manifest.32ab5b53cc88b2428c19ddb3668ea3b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-landing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
