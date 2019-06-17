bb72d2b06316c9f7e159e5e4925a8096d38b38a5
eruzanov <evgeniy.ruzanov@piano.io>
2019-06-17 17:51:28 +0300

    Updates

---
+++ precache-manifest.f5d9c3414e06264bb4e93f9a5b55fcfa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81038cbcbb27f199effe33cc0377c5cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911cc2a0b12a7da2ff81",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7b8aa9a3b026d8a7fc23",
+    "url": "/static/js/2.7f96c4b6.chunk.js"
+  },
+  {
+    "revision": "911cc2a0b12a7da2ff81",
+    "url": "/static/js/main.711160ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c135380ff4b27681b18e513538b97457",
+    "url": "/static/media/chart.c135380f.png"
+  },
+  {
+    "revision": "01dedef94ac390f8925705c71f388ee0",
+    "url": "/static/media/chart2.01dedef9.png"
+  },
+  {
+    "revision": "3b90db7e6d383d5edf21d37625939de6",
+    "url": "/static/media/chart3.3b90db7e.png"
+  },
+  {
+    "revision": "61bb53a468bd2d9ddac7f0839554bcdd",
+    "url": "/static/media/chart4.61bb53a4.png"
+  }
+]);
